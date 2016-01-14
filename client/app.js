ba69addd34dad75cde7ad4217dc17abc48920fa8
Alexander Wu <alexanderwu@Alexanders-MacBook-Pro.local>
2016-01-13 16:23:44 -0800

    [refactor] uses UI view to show multiple views on a page

--- client/app.js
+++ client/app.js
@@ -1,5 +1,34 @@
-var app = angular.module('app', ['signup', 'renderMap', 'ngAnimate'])
-  .controller('mapsPageController', ['$scope', '$http', function ($scope, $http){
+var app = angular.module('app', [
+  'signup', 
+  'renderMap',
+  'app.tweets',
+  'app.filter',
+  'app.search',
+  'ngAnimate', 
+  'ui.router',
+])
 
-  }]);
+.config(function ($stateProvider, $urlRouterProvider){
 
+$urlRouterProvider.otherwise("");
+
+// routing stuff
+$stateProvider
+.state('index', {
+      url: "",
+      views: {
+        'leftDiv': { 
+          controller: "tweetsController",
+          templateUrl: "client/views/tweetsView.html" 
+        },
+        'rightDiv': { 
+          controller: "filterController",
+          templateUrl: "client/views/filterListView.html" 
+        },
+        'searchbar': {
+          controller: "searchController",
+          templateUrl: "client/views/searchView.html"
+        }
+      }
+    });
+});
