8d835b04aebc6e8a0721efcdea1563405163749e
Alexander Wu <alexanderwu@Alexanders-MacBook-Pro.local>
2016-01-14 16:55:22 -0800

    [bug fixes] massage data to work with association engine

--- server/controllers/hashtagsController.js
+++ server/controllers/hashtagsController.js
@@ -1,24 +1,40 @@
 var model = require('../db/dbModel.js');
 var User = require('./usersController');
+var Promise = require('bluebird');
 
+// object will have the following format 
+// { hashtags: ['xyz', 'abc', ...], 
+//   name: { id: 'INTEGER', name: 'abc' } }
 var addHashtag = function (object) {
-  var userParams = { name: object.user };
-  var hashtagParams = { name: object.hashtags };
+  console.log("Adding hash tags: ");
+  console.dir(object);
+  var userParams = { name: object.name };
   
+  // find or create the user
   return User.addUser(userParams)
-    .then(function (user) {
-      model.Hashtag.findOrCreate({
-        where: hashtagParams, 
-        defaults: hashtagParams
+  .then(function (user) {
+    // once we have the user, iterate over the hashtags
+    return Promise.each(object.hashtags, function (hashtag){
+      // find or crate the hash tag
+      return model.Hashtag.findOrCreate({
+        where: {name: hashtag}, 
+        defaults: {name: hashtag},
       })
-      .spread(function (hashtag) {
-        var params = { hashtag_id: hashtag.id, user_id: user.id }
+      // once we have the hashtag object
+      .then(function (hashtag) {
+        console.log('hashtag.id: ' + hashtag[0].id);
+        console.log(hashtag);
+        console.log('user.name: ' + user[0].name);
+        console.log(user);
+        var params = { hashtag_id: hashtag[0].id, user_id: user[0].id }
+        // find or create an entry into the join table.
         return model.HashtagUser.findOrCreate({
           where: params,
           defaults: params
         });
       })
     })
+  });
 };
 
 var getHashtags = function (userId) {
--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -12,11 +12,11 @@ var model = require('../db/dbModel.js');
 // };
 
 var addUser = function (user) {
-  var params = { name: user.name }
+  var params = { name: user.name };
   return model.User.findOrCreate({
     where: params,
     defaults: params 
-  })
+  });
 };
 
 module.exports = {
